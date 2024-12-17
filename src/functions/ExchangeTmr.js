69b7589d294e8173122cb37a0c32d1160893fd20
camilonore <caminore22@gmail.com>
2024-12-17 13:27:06 -0500

    Changes

--- src/functions/ExchangeTmr.js
+++ src/functions/ExchangeTmr.js
@@ -1,10 +1,25 @@
-const { app } = require('@azure/functions')
+import { app } from '@azure/functions'
+import puppeteer from 'puppeteer'
+
+async function scrapPage() {
+  const browser = await puppeteer.launch({ headless: true })
+  const page = await browser.newPage()
+  const URL =
+    'https://www.uptc.edu.co/sitio/portal/sitios/universidad/rectoria/relinter/convoc/conv_sal/index.html'
+  await page.goto(URL)
+  const mainId = '#acco_daaa7622_1'
+  await page.waitForSelector(mainId)
+  const callsList = await page.evaluate((mainId) => {
+    return mainId
+  }, mainId)
+  console.log(callsList)
+  await browser.close()
+}
 
 app.timer('ExchangeTmr', {
-  schedule: '10 * * * * *',
-  handler
+  schedule: '1 * * * * *',
+  handler: async (myTimer, context) => {
+    context.log('Timer function processed request. Context.log')
+    await scrapPage()
+  }
 })
-
-const handler = (myTimer, context) => {
-  context.log('Timer function processed request.')
-}
